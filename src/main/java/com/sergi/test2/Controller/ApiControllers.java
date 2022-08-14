c33a7bb076d996c05a4138924b73cf69c1f73c58
Sergi Labadze <78698594+4blue2@users.noreply.github.com>
2022-08-14 17:38:39 +0100

    post method finally working!!!

--- src/main/java/com/sergi/test2/Controller/ApiControllers.java
+++ src/main/java/com/sergi/test2/Controller/ApiControllers.java
@@ -1,9 +1,11 @@
 package com.sergi.test2.Controller;
 
+import com.sergi.test2.Models.User;
 import com.sergi.test2.Repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ApiControllers {
@@ -16,4 +18,20 @@ public class ApiControllers {
         return "Welcome my Master... What shall I do for you?";
     }
 
+    //Return type will be a list of user(from user.java)
+    @GetMapping(value = "/users")
+    public List<User> getUsers(){
+        return userRepo.findAll();
+    }
+
+    @GetMapping(value = "/test")
+    public String getTest(){
+        return "Test successful";
+    }
+
+    @PostMapping(value = "/save")
+    public String saveUser(@RequestBody User user){
+        userRepo.save(user);
+        return "saved ...";
+    }
 }
